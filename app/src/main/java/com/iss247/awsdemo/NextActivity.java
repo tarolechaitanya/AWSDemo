281553ed10e04260c147661670216ca3a9f18bdf
Chaitanya Tarole <chaitanya.tarole@gmail.com>
2018-04-11 12:59:11 +0530

    Adding AWS Demo project

--- app/src/main/java/com/iss247/awsdemo/NextActivity.java
+++ app/src/main/java/com/iss247/awsdemo/NextActivity.java
@@ -1,5 +1,7 @@
 package com.iss247.awsdemo;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class NextActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
